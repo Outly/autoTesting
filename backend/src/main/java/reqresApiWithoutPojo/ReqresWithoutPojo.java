44896eb632a94e94417825ded816bfc986e54764
Outly <kamenschool41@yandex.ru>
2022-07-06 13:43:55 +0600

    add without pojo test part 2

--- backend/src/main/java/reqresApiWithoutPojo/ReqresWithoutPojo.java
+++ backend/src/main/java/reqresApiWithoutPojo/ReqresWithoutPojo.java
@@ -4,16 +4,19 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresWithoutPojo {
+    String URL = "https://reqres.in/";
 
     @Test
     public void checkAvatarsTest() {
-        String URL = "https://reqres.in/";
 
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpec(200));
 
@@ -42,5 +45,58 @@ public class ReqresWithoutPojo {
         }
 
         Assert.assertTrue(emails.stream().allMatch(x -> x.endsWith("@reqres.in")));
+    }
+
+    @Test
+    public void successRegWithoutPojoTest() {
+
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpec(200));
+
+        // Создаём словарь для переноса в него данных из json
+        Map<String, String> user = new HashMap<>();
+        user.put("email", "eve.holt@reqres.in");
+        user.put("password", "pistol");
+
+        // Создание POST запроса без создания экземпляра класса Response
+        given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .body("id", equalTo(4))
+                .body("token", equalTo("QpwL5tke4Pnpja7X4"));
+
+        // Делаем тоже самое, но с созданием response
+        Response response = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(Optional.of(4),Optional.of(jsonPath.get("id")));
+        Assert.assertEquals("QpwL5tke4Pnpja7X4", jsonPath.get("token"));
+    }
+
+    @Test
+    public void unSuccessRegWithoutPojoTest() {
+
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpec(400));
+
+        Map<String, String> userLoozer = new HashMap<>();
+        userLoozer.put("email", "sydney@fife");
+
+        Response response = given()
+                .body(userLoozer)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(jsonPath.get("error"), "Missing password");
     }
 }
